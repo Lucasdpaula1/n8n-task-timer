4b325c92a72af5da42c07d2b34cf3957dfc4da76
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-03-12 20:49:11 -0300

    feat: padrões de UI, cores e configs

--- dist/modules/teste.js
+++ dist/modules/teste.js
@@ -1,6 +1,1 @@
-export const teste = () => { };
-const button = document.querySelector("button");
-function darOla() {
-    console.log("ola");
-}
-button?.addEventListener("click", darOla);
+export {};
